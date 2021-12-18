b0c6ec1169b01c94dcdb4dbf52667ef563c7b970
lochie <disrute@gmail.com>
2021-12-19 10:13:37 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def factorial(n):
-    print(n)
-    return n * factorial(n -1)
+    assert n is int and n >= 0, 'number must be a positive integer'
+    if n in [0,1]:
+        return 1
+    else:
+        return n * factorial(n -1) 
+    
 
-factorial(3)
+print(factorial(-1))
