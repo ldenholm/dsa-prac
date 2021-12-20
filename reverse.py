e599325a4af6560206944fee1bc1b67ad1b66909
lochie <disrute@gmail.com>
2021-12-20 15:13:55 +1100

    add queue implementation, its all order of append and pop

--- reverse.py
+++ reverse.py
@@ -12,10 +12,10 @@ reverse('tnerruc')
 class Stack:
     data = []
 
-    def Pop(self):
+    def Remove(self):
         return self.data.pop()
     
-    def Append(self, data):
+    def Add(self, data):
         self.data.append(data)
 
     def Print(self):
@@ -26,15 +26,42 @@ class Stack:
     def Find(self, index):
         return self.data[index]
 
+        # by appending then popping we are always adding to end of list, but equally always removing from end of list via pop
+
 def StackTester():
     stack = Stack
 
-    stack.Append(stack, "cool")
-    stack.Append(stack, "second")
+    stack.Add(stack, "cool")
+    stack.Add(stack, "second")
     stack.Print(stack)
     print(stack.Find(stack, 1))
-    print('popping: ', stack.Pop(stack))
+    print('popping: ', stack.Remove(stack))
     stack.Print(stack)
 
 
 StackTester()
+
+
+class Queue:
+    data = []
+
+    def Add(self, data):
+        self.data.append(data)
+
+    def Remove(self):
+        return self.data.pop(0)
+    
+    def Print(self):
+        for x in self.data:
+            print(x, 'at index', self.data.index(x))
+
+def QueueTester():
+    q = Queue
+    q.Add(q, "first")
+    q.Add(q, "2nd")
+    q.Add(q, "third3333")
+    q.Print(q)
+    q.Remove(q)
+    q.Print(q)
+
+QueueTester()   
