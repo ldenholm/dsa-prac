b44e2058c0be4b3d3b988e324692bb57c372c0c0
lochie <lochie.denholm@carsales.com.au>
2021-12-27 19:21:11 +1100

    add greater than x func

--- lists/smaller_than_x.py
+++ lists/smaller_than_x.py
@@ -1,5 +1,5 @@
 # A function that takes an array and a given x,
-# that returns a list with all numbers 
+# that returns a list with all numbers strictly
 # smaller than x. P.s. nice syntax.
 
 def smaller_than_x(arr, x):
@@ -10,3 +10,9 @@ l = [1, 2, 7, 9, 10]
 x = 9
 print(smaller_than_x(l, x))
 
+# Same as above except returns all numbers
+# strictly greater than x.
+def greater_than_x(arr, x):
+    l = [i for i in arr if i > x]
+    return l
+print(greater_than_x(l, x))
