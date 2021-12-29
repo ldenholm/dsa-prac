740d194b2f937951f68c85e0f1ba02457047f3e2
lo den <disrute@gmail.com>
2021-12-29 15:06:42 +1100

    add rotate with loop

--- lists/rotations/left_rotate_by_one.py
+++ lists/rotations/left_rotate_by_one.py
@@ -6,4 +6,15 @@ def rotate(arr):
     # arr.append(aa.pop(0))
 
 l1 = [5, 10, 20 , 3]
-rotate(l1)
+#rotate(l1)
+
+# using a loop
+def rotate_loop(arr):
+    last = arr[0]
+    for i in range(1, len(arr)):
+        arr[i - 1] = arr [i]
+        print(arr)
+    arr[len(arr)-1] = last
+    print(arr)
+
+rotate_loop(l1)
