37ef7269d3d52437f26d474fbc8343439f9d7ce0
lo den <disrute@gmail.com>
2022-01-02 12:11:54 +1100

    add dict and pangram checker

---
+++ hashing/dicts.py
@@ -0,0 +1,9 @@
+# Dict aka hash table aka hash map
+def simpleDict(input):
+    # we can define a hash table like so:
+    ht = {}
+    ht['a'] = 1
+    print(ht.get('a'))
+
+
+simpleDict('nothing')
---
+++ string_manip/pangram_checking.py
@@ -0,0 +1,7 @@
+import string
+def pan(s):
+    alphabetList = list(string.ascii_lowercase)
+    d = {}
+    for x in alphabetList:
+        d[x] = 0
+pan('s')
