b33f7329a23b7b1638e05089a918b7809c9ab88c
lochie <lochie.denholm@carsales.com.au>
2022-01-05 10:39:35 +1100

    adding set info

--- sets/set_comprehension.py
+++ sets/set_comprehension.py
@@ -4,3 +4,12 @@ s1 = {x for x in l if x % 2 == 0}
 print(s1)
 s2 = {x for x in l if x % 2 != 0}
 print(s2)
+
+print(type(s1))
+print(type(s2))
+
+my_set = {10, 20, 30}
+print(type(my_set))
+
+rando_dict = {i : 'cool' for i in s1}
+print(type(rando_dict))
