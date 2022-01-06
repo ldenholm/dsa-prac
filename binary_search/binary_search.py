883ea6b1d9963b606165e73b269aeefe7cbe56da
lo den <disrute@gmail.com>
2022-01-06 13:38:24 +1100

    condition for while loop changed to lower < upper

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,13 +1,9 @@
 def binary_search(arr, x):
-    # n = last index in array
     n = len(arr) - 1
     lower = 0
-    mid = n // 2
-    print(n)
-    print(mid)
+    mid = (lower + n) // 2
     i = 0
-    #for i in range(len(arr)):
-    while i < len(arr):
+    while lower <= n:
         if x == arr[mid]:
             return mid
         elif x > arr[mid]:
@@ -16,11 +12,12 @@ def binary_search(arr, x):
         elif x <= arr[mid]:
             n = mid - 1
             mid = (lower + n) // 2
-        i += 1
     return -1
 
  #    0     m    n-1
+ #    0  1  2  3  4
 t0 = [1, 2, 3, 4, 5]
 #t1 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 x = 2
 print(binary_search(t0, 4))
+print(binary_search(t0, 5))
