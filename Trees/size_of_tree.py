47135f4605bf894e0f8c3ff2c1e76e8e55fd86d2
lo den <disrute@gmail.com>
2022-01-07 15:15:08 +1100

    return size of binary tree

---
+++ Trees/size_of_tree.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, k):
+        self.left = None
+        self.right = None
+        self.key = k
+
+
+def size_of_binary_tree(node):
+    size = 0
+    if node == None:
+        return 0
+    elif node.key != None:
+        # recursively call left
+        size += size_of_binary_tree(node.left)
+        size += 1
+        size += size_of_binary_tree(node.right)
+    return size
+
+def concise_size_of_binary_tree(node):
+    if node == None:
+        return 0
+    else:
+        leftsubtree = concise_size_of_binary_tree(node.left)
+        rightsubtree = concise_size_of_binary_tree(node.right)
+        return leftsubtree + rightsubtree + 1         
+
+tree = Node(10)
+tree.left = Node(5)
+tree.left.left = Node(1)
+tree.right = Node(20)
+print(size_of_binary_tree(tree))
+
+print(concise_size_of_binary_tree(tree))
+
+#    10
+#   /  \
+#  5   20
+# /
+#1
