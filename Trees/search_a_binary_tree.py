7d8fa6e94c2cc70dd1ec569ba05a43a5d4883656
lo den <disrute@gmail.com>
2022-01-09 18:54:37 +1100

    add search binary tree function

---
+++ Trees/search_a_binary_tree.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, k):
+        self.left = None
+        self.right = None
+        self.val = k
+
+def search_tree(root, searchval):
+    # return T/F for existence of node in tree
+    if not root:
+        return False
+    elif root.val == searchval:
+        return True
+    else:
+        exists_left = search_tree(root.left, searchval)
+        exists_right = search_tree(root.right, searchval)
+        return exists_left or exists_right
+    
+tree = Node(60)
+tree.left = Node(40)
+tree.left.left = Node(2)
+tree.left.right = Node(16)
+tree.right = Node(80)
+tree.right.left = Node(64)
+tree.right.right = Node(84)
+
+print(search_tree(tree, 99))
+print(search_tree(tree, 2))
+print(search_tree(tree, 84))
+print(search_tree(tree, 82))
